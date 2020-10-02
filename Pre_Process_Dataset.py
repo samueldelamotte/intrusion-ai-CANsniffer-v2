00965bd5f238ef6073a5fbc4d78779cdbec06d3a
Samuel De La Motte <samuel.delamotte1@gmail.com>
2020-10-02 19:42:37 +0800

    Processed data is now separated into passive and aggressive

--- Pre_Process_Dataset.py
+++ Pre_Process_Dataset.py
@@ -27,32 +27,39 @@ def process(data, file):
 
 
 if __name__ == '__main__':
-    df = pd.read_csv(r'Dataset\Processed\dataset.csv', index_col=None, header=0)
-    x = len(df)
-    AGGRESSIVE_DIR = r'Dataset\Aggressive'
-    PASSIVE_DIR = r'Dataset\Passive'
-    SAVE_DIR = r'Dataset\Processed'
+    AGGRESSIVE_DIR = os.path.join('Dataset', 'Aggressive')
+    PASSIVE_DIR = os.path.join('Dataset', 'Passive')
+    PASSIVE_SAVE_DIR = os.path.join('Dataset', 'Processed', 'Passive')
+    AGGRESSIVE_SAVE_DIR = os.path.join('Dataset', 'Processed', 'Aggressive')
+    aggressive_logs = glob.glob(os.path.join(AGGRESSIVE_DIR, '*.csv'))
+    passive_logs = glob.glob(os.path.join(PASSIVE_DIR, '*.csv'))
 
-    aggressive_logs = glob.glob(AGGRESSIVE_DIR + '/*.csv')
-    passive_logs = glob.glob(PASSIVE_DIR + '/*.csv')
-
-    # Concatenate all logs
-    tmp = []
+    # Concatenate passive logs
+    passiveTmp = []
     for file in passive_logs:
         df = pd.read_csv(file, index_col=None, header=0)
         del df['time_stamp']                                    # Delete time_stamp column as it is not necessary
         df['class_label'] = '0'                                 # Add new column with class_label, 0 => passive
         df_processed = process(df, file)                        # Process data
-        tmp.append(df_processed)                                # Append to temporary list
+        passiveTmp.append(df_processed)                         # Append to temporary list
+    df1 = pd.concat(passiveTmp, axis=0, ignore_index=True)
+
+    # Concatenate aggressive logs
+    aggressiveTmp = []
     for file in aggressive_logs:
         df = pd.read_csv(file, index_col=None, header=0)
         del df['time_stamp']                                    # Delete time_stamp column as it is not necessary
         df['class_label'] = '1'                                 # Add new column with class_label, 1 => aggressive
         df_processed = process(df, file)                        # Process data
-        tmp.append(df_processed)                                # Append to temporary list
-    df = pd.concat(tmp, axis=0, ignore_index=True)
+        aggressiveTmp.append(df_processed)                      # Append to temporary list
+    df2 = pd.concat(aggressiveTmp, axis=0, ignore_index=True)
 
-    # Save pre-processed concatenated logs to one file
-    if not os.path.exists(os.path.join(SAVE_DIR)):
-        os.mkdir(os.path.join(SAVE_DIR))
-    df.to_csv(SAVE_DIR + r'\dataset.csv', index=False)
+    # Save pre-processed passive concatenated logs to one file
+    if not os.path.exists(PASSIVE_SAVE_DIR):
+        os.mkdir(os.path.join(PASSIVE_SAVE_DIR))
+    df1.to_csv(os.path.join(PASSIVE_SAVE_DIR, 'passive-dataset.csv'), index=False)
+
+    # Save pre-processed aggressive concatenated logs to one file
+    if not os.path.exists(os.path.join(AGGRESSIVE_SAVE_DIR)):
+        os.mkdir(os.path.join(AGGRESSIVE_SAVE_DIR))
+    df2.to_csv(os.path.join(AGGRESSIVE_SAVE_DIR, 'aggressive-dataset.csv'), index=False)
